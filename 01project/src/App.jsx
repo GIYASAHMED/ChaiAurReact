b828f8f865d83913d50f94aba86c8d9b8a09fefa
giyas <giyas.bk@gmail.com>
2023-10-08 07:58:10 +0530

    all config set

---
+++ 01project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>HELLO WORLD WITH CHAI AUR CODE</h1>
+      <p>Welcome to my react project First</p>
+      </>
+  )
+}
+
+export default App
